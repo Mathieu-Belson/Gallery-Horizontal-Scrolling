83f2dd068c29a11567b431a9bbe338378503c1a7
mathieu belson <mathieu.belson@gmail.com>
2023-09-19 23:26:35 +0900

    clic event created for the buttons

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-let scrollContainer = document.querySelector('.gallery');
-let backButton = document.getElementById('backButton');
-let nextButton = document.getElementById('nextButton');
+let scrollContainer = document.querySelector(".gallery");
+let backButton = document.getElementById("backButton");
+let nextButton = document.getElementById("nextButton");
 
-scrollContainer.addEventListener('wheel', (event) => {
-event.preventDefault();
-scrollContainer.scrollLeft += event.deltaY; /* event.deltaY is the amount of pixels the user scrolled vertically */
+scrollContainer.addEventListener("wheel", (evt) => {
+evt.preventDefault();
+scrollContainer.scrollLeft += evt.deltaY; /* event.deltaY is the amount of pixels the user scrolled vertically */
 });
 
-nextButton.addEventListener('click', () => {
+nextButton.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = "smooth";
     scrollContainer.scrollLeft += 900; /* because the width of the gallery is 900px an + because we want to go forward */  
+    console.log('next');
 });
 
-backButton.addEventListener('click', () => {
-    scrollContainer.scrollLeft -= 900; /* minus because we want to go back */  
-}); 
+backButton.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft -= 900; /* minus because we want to go back */
+    console.log('back');
+});     
