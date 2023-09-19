14301245c71b7d30a67055a75d2856d99fd21c0e
mathieu belson <mathieu.belson@gmail.com>
2023-09-19 14:54:48 +0900

    image gallery set horizontally, back and next buttons added, scrolling function underrcoy set, back and next buttons added, transition added with hover

---
+++ script.js
@@ -0,0 +1,7 @@
+let scrollContainer = document.querySelector('.gallery');
+let backButton = document.getElementById('backButton');
+let nextButton = document.getElementById('nextButton');
+
+scrollContainer.addEventListener('wheel', (event) => {
+event
+});
