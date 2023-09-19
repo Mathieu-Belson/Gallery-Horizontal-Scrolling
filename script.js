1fce3842374b4235c35b3dd80b21a41180879d5c
mathieu belson <mathieu.belson@gmail.com>
2023-09-19 15:06:12 +0900

    wheel scrolling function created

--- script.js
+++ script.js
@@ -3,5 +3,14 @@ let backButton = document.getElementById('backButton');
 let nextButton = document.getElementById('nextButton');
 
 scrollContainer.addEventListener('wheel', (event) => {
-event
+event.preventDefault();
+scrollContainer.scrollLeft += event.deltaY; /* event.deltaY is the amount of pixels the user scrolled vertically */
 });
+
+nextButton.addEventListener('click', () => {
+    scrollContainer.scrollLeft += 900; /* because the width of the gallery is 900px an + because we want to go forward */  
+});
+
+backButton.addEventListener('click', () => {
+    scrollContainer.scrollLeft -= 900; /* minus because we want to go back */  
+}); 
